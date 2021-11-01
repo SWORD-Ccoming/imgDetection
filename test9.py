804245d5a77a6ca89c787b3e62ec1d52a174b7aa
cj <1004747472@qq.com>
2021-11-01 21:02:33 +0800

    变量G变为G3

--- test9.py
+++ test9.py
@@ -2,5 +2,5 @@ import cv2
 import numpy as np
 dataBGR = cv2.imread('color.png')
 dataRGB = cv2.cvtColor(dataBGR, cv2.COLOR_BGR2RGB)
-R,G2,B = cv2.split(dataRGB)
+R,G3,B = cv2.split(dataRGB)
 print(R.max(), np.min(R))
